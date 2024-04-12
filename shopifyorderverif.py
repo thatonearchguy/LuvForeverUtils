8cc396ba1844a9f4c37e00a8fd52b656ccb99f66
Yuvraj D <67145962+thatonearchguy@users.noreply.github.com>
2024-04-12 15:11:27 +0100

    added sold at column

--- shopifyorderverif.py
+++ shopifyorderverif.py
@@ -14,6 +14,7 @@ def match_yumi_orders_to_shopify(yumi_df, shopify_df, shopify_product_df):
     shopify_df['YumiMatch'] = ""
     shopify_df['PriceDelta'] = ""
     shopify_df['Actual RRP'] = ""
+    shopify_df['Sold at'] = ""
     for index, row in yumi_df.iterrows():
         found_indices = shopify_df['Name'][shopify_df['Name'] == str(row['Host Order Ref'])].index.tolist()
         if(len(found_indices) == 1):
@@ -21,6 +22,8 @@ def match_yumi_orders_to_shopify(yumi_df, shopify_df, shopify_product_df):
             shopify_df['PriceDelta'][found_indices] = actual_price - row['Item Price']
             shopify_df['YumiMatch'][found_indices] = "YES"
             shopify_df['Actual RRP'][found_indices] = actual_price
+            shopify_df['Sold at'][found_indices] = row['Item Price']
+            shopify
         if(len(found_indices) > 1):
             for mult_indices in found_indices:
                 if shopify_df['Lineitem sku'][mult_indices] == row['Variant Code']:
@@ -28,8 +31,8 @@ def match_yumi_orders_to_shopify(yumi_df, shopify_df, shopify_product_df):
                     shopify_df['PriceDelta'][mult_indices] = actual_price - row['Item Price']
                     shopify_df['YumiMatch'][mult_indices] = "YES"
                     shopify_df['Actual RRP'][mult_indices] = actual_price
-
                     break
+                    shopify_df['Sold at'][found_indices] = row['Item Price']
 
     shopify_df.drop(shopify_df.columns[list(range(21, 75))], axis=1, inplace=True)
     shopify_df.drop(shopify_df.columns[[3, 6, 12, 14, 18, 19]], axis=1, inplace=True)
